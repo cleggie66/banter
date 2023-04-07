b57ed134f00b2b7209cdd657a59bf50749d9112d
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-04-07 16:03:57 -0700

    combined workspace seeder to match workspace model

--- app/seeds/workspaces.py
+++ app/seeds/workspaces.py
@@ -1,4 +1,4 @@
-from app.models import db, Workspace, environment, SCHEMA
+from app.models import db, Workspace, WorkspaceUser, environment, SCHEMA
 from sqlalchemy.sql import text
 
 def seed_workspaces():
@@ -26,5 +26,25 @@ def undo_workspaces():
         db.session.execute(f"TRUNCATE table {SCHEMA}.workspaces RESTART IDENTITY CASCADE;")
     else:
         db.session.execute(text("DELETE FROM workspaces"))
+
+    db.session.commit()
+
+def seed_workspace_users():
+
+    workspace_user1 = WorkspaceUser(user_id=1, workspace_id=3)
+    workspace_user2 = WorkspaceUser(user_id=2, workspace_id=2)
+    workspace_user3 = WorkspaceUser(user_id=3, workspace_id=1)
+
+    db.session.add(workspace_user1)
+    db.session.add(workspace_user2)
+    db.session.add(workspace_user3)
+    db.session.commit()
+
+
+def undo_worspace_users():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.workspace_users RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute(text("DELETE FROM workspace_users"))
 
     db.session.commit()
