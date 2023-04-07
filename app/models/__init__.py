3c04aa2d02563072aacb49ad99439cd8e0699722
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-04-07 12:59:53 -0700

    updated init model structure

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,7 @@
 from .db import db
 from .user import User
 from .db import environment, SCHEMA
+from .channel import Channel
+from .workspace import Workspace, WorkspaceUser
+from .message import Message
+from .group import Group, GroupMember
