6174619d40129401bf6a0b41eaf3ac7981b0162d
spencerwilf <spencerwilf@yahoo.com>
2023-04-10 13:26:19 -0700

    channels create

--- app/api/channel_routes.py
+++ app/api/channel_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, redirect, request
 from app.models import Channel, User, channel_member, db
+from app.models.channel_member import channel_members
 from flask_login import current_user, login_required
 from flask_wtf.csrf import CSRFProtect, generate_csrf
 from ..forms.channel_form import ChannelForm
@@ -20,18 +21,28 @@ def get_single_channels(channel_id):
 def get_all_channels():
     user = User.query.filter(User.id == current_user.id).first()
     channels = user.joined_channels
-    return channels
+    return [channel.to_dict() for channel in channels]
 
 
 @channel_routes.route('/create-channel', methods=['GET','POST'])
 @login_required
 def create_channel():
+    print(current_user)
     form = ChannelForm()
     form['csrf_token'].data = request.cookies['csrf_token']
     if form.validate_on_submit():
-        new_channel = Channel()
-        form.populate_obj(new_channel)
+
+
+        new_channel = Channel(
+            name=form.data['name'],
+            workspace_id = form.data['workspace_id'],
+            is_channel = form.data['is_channel'],
+            users_in_channels = [current_user]
+        )
+
         db.session.add(new_channel)
         db.session.commit()
+
+
         return redirect(f"/channels/{new_channel.id}")
     return 'BAD DATA'
