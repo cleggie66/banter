a8a743976e912fc80f7cac64dad2af91f4bf7682
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-04-10 11:03:08 -0700

    added join table seeders for channels

--- app/seeds/channels.py
+++ app/seeds/channels.py
@@ -1,20 +1,20 @@
 from app.models import db, Channel, environment, SCHEMA
 from sqlalchemy.sql import text
-
+from .users import demo, alec, brad
 
 def seed_channels():
     general = Channel(
-        name='general', workspace_id=1, is_channel=True)
+        name='general', workspace_id=1, is_channel=True, users_in_channels=[demo, alec, brad] )
     november = Channel(
-        name='2022-11-21-online', workspace_id=1, is_channel=True)
+        name='2022-11-21-online', workspace_id=1, is_channel=True, users_in_channels=[demo, alec, brad])
     lecture = Channel(
-        name='2022-11-21-lecture-questions', workspace_id=1, is_channel=True)
+        name='2022-11-21-lecture-questions', workspace_id=1, is_channel=True, users_in_channels=[demo, alec, brad])
     project = Channel(
-        name='lecture questions', workspace_id=1, is_channel=True)
+        name='lecture questions', workspace_id=1, is_channel=True, users_in_channels=[demo, alec, brad])
     dm = Channel(
-        name='direct message', workspace_id=1, is_channel=False)
+        name='direct message', workspace_id=1, is_channel=False, users_in_channels=[demo, brad])
     dgm = Channel(
-        name='direct group message', workspace_id=1, is_channel=False)
+        name='direct group message', workspace_id=1, is_channel=False, users_in_channels=[demo, alec, brad])
 
     db.session.add(general)
     db.session.add(november)
@@ -23,7 +23,6 @@ def seed_channels():
     db.session.add(dm)
     db.session.add(dgm)
     db.session.commit()
-
 
 def undo_channels():
     if environment == "production":
--- app/seeds/users.py
+++ app/seeds/users.py
@@ -3,14 +3,14 @@ from sqlalchemy.sql import text
 
 
 # Adds a demo user, you can add other users here if you want
-def seed_users():
-    demo = User(
-        username='Demo', password='password', email='demo@aa.io', first_name="Demo", last_name="Lition", profile_picture="https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png", title="Demo User", about_me="I am a demo user testintg this application!" )
-    alec = User(
-        username='Alec', password='password', email='mod4god@aa.io', first_name="Alec", last_name="Instructor", profile_picture="https://i0.wp.com/d37ck3jytu9wl0.cloudfront.net/wp-content/uploads/2022/05/31141519/Neckbeard-BLOG.jpg?fit=1200%2C777&ssl=1", title="Mod 4 Gate Keeper", about_me="I am the greatest mod 4 instructor of all time")
-    brad = User(
-        username='Brad',  password='password', email='Brad@aa.io', first_name="Brad", last_name="Instructor", profile_picture="https://cdn.vox-cdn.com/thumbor/qqJR3THHUSgAt2ADTGk6_56hjyQ=/0x0:4059x4051/1400x1400/filters:focal(1964x2333:1965x2334)/cdn.vox-cdn.com/uploads/chorus_asset/file/24018802/ND_Zelda_Lead.jpeg", title="Food variable master", about_me="The father of Patches and Blue")
+demo = User(
+    username='Demo', password='password', email='demo@aa.io', first_name="Demo", last_name="Lition", profile_picture="https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png", title="Demo User", about_me="I am a demo user testintg this application!" )
+alec = User(
+    username='Alec', password='password', email='mod4god@aa.io', first_name="Alec", last_name="Instructor", profile_picture="https://i0.wp.com/d37ck3jytu9wl0.cloudfront.net/wp-content/uploads/2022/05/31141519/Neckbeard-BLOG.jpg?fit=1200%2C777&ssl=1", title="Mod 4 Gate Keeper", about_me="I am the greatest mod 4 instructor of all time")
+brad = User(
+    username='Brad',  password='password', email='Brad@aa.io', first_name="Brad", last_name="Instructor", profile_picture="https://cdn.vox-cdn.com/thumbor/qqJR3THHUSgAt2ADTGk6_56hjyQ=/0x0:4059x4051/1400x1400/filters:focal(1964x2333:1965x2334)/cdn.vox-cdn.com/uploads/chorus_asset/file/24018802/ND_Zelda_Lead.jpeg", title="Food variable master", about_me="The father of Patches and Blue")
 
+def seed_users():
     db.session.add(demo)
     db.session.add(alec)
     db.session.add(brad)
