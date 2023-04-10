5683e61da6cc2b7f660c3a697ec407f3f9988d84
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-04-10 13:41:48 -0700

    added delete route for channel

--- app/api/channel_routes.py
+++ app/api/channel_routes.py
@@ -4,3 +4,29 @@ from app.models import Channel
 channel_routes = Blueprint('channels', __name__)
 
 # FULL CRUD
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ! DELETE
+@channel_routes.route('/<int:id>', methods=['DELETE'])
+@login_required
+def delete_channel_by_id(id):
+    channel = Channel.query.get(id)
+
+    db.session.delete(channel)
+    db.session.commit()
+
+    return {"message": "Successfully Deleted!"}
