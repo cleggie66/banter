5b060ad2d3502d342e3ad8f2355b691a30e8e237
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-04-09 17:17:49 -0700

    changed imports for models init file

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,7 +1,5 @@
-from .db import db
-from .user import User
-from .db import environment, SCHEMA
+from .db import db, environment, SCHEMA
 from .channel import Channel
-from .workspace import Workspace, WorkspaceUser
-from .message import DirectMessage, GroupMessage
-from .group import Group, GroupMember
+from .user import User
+from .workspace import Workspace
+from .message import Message
