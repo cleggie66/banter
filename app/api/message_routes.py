edd581feef827cd68e84bdaaa31ac55d42b3f5da
spencerwilf <spencerwilf@yahoo.com>
2023-04-10 20:34:20 -0700

    message read, update, create

--- app/api/message_routes.py
+++ app/api/message_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, redirect
 from flask_login import current_user, login_required
 from ..forms.message_form import MessageForm
 from app.models import Message, db, Channel
@@ -29,6 +29,32 @@ message_routes = Blueprint('messages', __name__)
 #         return redirect(f"/channels/{new_channel.id}")
 #     return 'BAD DATA'
 
+# @channel_routes.route('/<int:channel_id>', methods=['PUT','PATCH'])
+# @login_required
+# def update_channel(channel_id):
+#     edit = request.json
+
+#     channel = Channel.query.get(channel_id)
+#     workspace_owner = channel.channel_in_workspace.owner_id
+
+#     if (channel.is_channel == True and workspace_owner != current_user.id):
+#         return {
+#             "message": "Unauthorized"
+#         }, 401
+
+#     channel.name = edit['name']
+#     db.session.commit()
+#     return redirect(f"/channels/{channel.id}")
+
+
+@message_routes.route('/<message_id>', methods=['PUT'])
+@login_required
+def update_message(message_id):
+    edit = request.json
+    message = Message.query.get(message_id)
+    message.content = edit['content']
+    db.session.commit()
+    return redirect(f"/channels/{message.id}")
 
 
 @message_routes.route('', methods=['POST'])
@@ -52,12 +78,10 @@ def create_message():
 
 
 @message_routes.route('/<channel_id>')
+@login_required
 def get_channel_messages(channel_id):
     messages = Message.query.filter(Message.channel_id == channel_id)
     return [message.to_dict_simple() for message in messages]
 
 
-@message_routes.route('/<message_id>', methods=['PUT'])
-def update_message(message_id):
-    message = Message.query.get(message_id)
-    return message.to_dict_simple()
+@message_routes.route('/<message_id>')
