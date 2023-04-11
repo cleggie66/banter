60b56420045809670d070b854ae57ffcbf4c654f
JakeG97 <jgularteee@gmail.com>
2023-04-10 18:50:40 -0700

    read all workspaces route created and working

--- app/api/workspace_routes.py
+++ app/api/workspace_routes.py
@@ -1,7 +1,19 @@
 from flask import Blueprint
-from app.models import Workspace
+from app.models import Workspace, User, workspace_member, db
+from app.models.workspace_member import workspace_members
+from flask_login import current_user, login_required
 
 
 workspace_routes = Blueprint('workspaces', __name__)
 
 # CREATE AND READ
+
+# * -----------  GET  --------------
+# Returns all Workspaces that the user is a part of
+
+@workspace_routes.route('')
+@login_required
+def get_all_workspaces():
+    user = User.query.filter(User.id == current_user.id).first()
+    workspaces = user.joined_workspaces
+    return [workspace.to_dict_simple() for workspace in workspaces]
