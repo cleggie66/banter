91d3be0c16f6bc2a85b441b9d455ffdf99c94f2f
JakeG97 <jgularteee@gmail.com>
2023-04-11 10:33:07 -0700

    update user working

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,25 +1,49 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, request
 from flask_login import login_required
 from app.models import User
+from app import db
 
 user_routes = Blueprint('users', __name__)
 
 
+# * -----------  GET  --------------
+#Query for all users and returns them in a list of user dictionaries
+
 @user_routes.route('/')
 @login_required
 def users():
-    """
-    Query for all users and returns them in a list of user dictionaries
-    """
+
     users = User.query.all()
     return {'users': [user.to_dict() for user in users]}
 
 
+# * -----------  GET  --------------
+# Query for a user by id and returns that user in a dictionary
+
 @user_routes.route('/<int:id>')
 @login_required
 def user(id):
-    """
-    Query for a user by id and returns that user in a dictionary
-    """
+
     user = User.query.get(id)
+    return user.to_dict()
+
+
+# TODO -----------  PUT  --------------
+#  Update a user by id and returns that user in a dictionary
+
+@user_routes.route('/<int:id>', methods=['PUT'])
+@login_required
+def update_user(id):
+
+    user = User.query.get(id)
+
+    if not user:
+        return 'User not found', 404
+
+    user.username = request.json.get('username', user.username)
+    user.email = request.json.get('email', user.email)
+    user.first_name = request.json.get('first_name', user.first_name)
+    user.last_name = request.json.get('last_name', user.last_name)
+    user.password = request.json.get('password', user.password)
+    db.session.commit()
     return user.to_dict()
