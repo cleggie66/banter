aee0cf0c2168813ba042f69cf734baac8a96d70d
spencerwilf <spencerwilf@yahoo.com>
2023-04-10 20:52:10 -0700

    mostly finished message crud

--- app/api/message_routes.py
+++ app/api/message_routes.py
@@ -6,47 +6,6 @@ from app.models import Message, db, Channel
 message_routes = Blueprint('messages', __name__)
 
 
-# FULL CRUD
-
-# @channel_routes.route('', methods=['GET','POST'])
-# @login_required
-# def create_channel():
-#     form = ChannelForm()
-#     form['csrf_token'].data = request.cookies['csrf_token']
-#     if form.validate_on_submit():
-
-#         new_channel = Channel(
-#             name=form.data['name'],
-#             workspace_id = form.data['workspace_id'],
-#             is_channel = form.data['is_channel'],
-#             users_in_channels = [current_user]
-#         )
-
-#         db.session.add(new_channel)
-#         db.session.commit()
-
-
-#         return redirect(f"/channels/{new_channel.id}")
-#     return 'BAD DATA'
-
-# @channel_routes.route('/<int:channel_id>', methods=['PUT','PATCH'])
-# @login_required
-# def update_channel(channel_id):
-#     edit = request.json
-
-#     channel = Channel.query.get(channel_id)
-#     workspace_owner = channel.channel_in_workspace.owner_id
-
-#     if (channel.is_channel == True and workspace_owner != current_user.id):
-#         return {
-#             "message": "Unauthorized"
-#         }, 401
-
-#     channel.name = edit['name']
-#     db.session.commit()
-#     return redirect(f"/channels/{channel.id}")
-
-
 @message_routes.route('/<message_id>', methods=['PUT'])
 @login_required
 def update_message(message_id):
@@ -57,6 +16,7 @@ def update_message(message_id):
     return redirect(f"/channels/{message.id}")
 
 
+#NEEDS WORK
 @message_routes.route('', methods=['POST'])
 @login_required
 def create_message():
@@ -84,4 +44,10 @@ def get_channel_messages(channel_id):
     return [message.to_dict_simple() for message in messages]
 
 
-@message_routes.route('/<message_id>')
+@message_routes.route('/<message_id>', methods=['DELETE'])
+@login_required
+def delete_message(message_id):
+    message = Message.query.get(message_id)
+    db.session.delete(message)
+    db.session.commit()
+    return {"message": "Successfully Deleted!"}
