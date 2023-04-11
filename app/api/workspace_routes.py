cecd490af3e7b221c61f4fda1c8a20baa5e7a892
JakeG97 <jgularteee@gmail.com>
2023-04-10 18:58:47 -0700

    read one workplace working

--- app/api/workspace_routes.py
+++ app/api/workspace_routes.py
@@ -9,6 +9,15 @@ workspace_routes = Blueprint('workspaces', __name__)
 # CREATE AND READ
 
 # * -----------  GET  --------------
+# Returns the details of a workspace specified by its Id
+@workspace_routes.route("/<int:workspace_id>")
+@login_required
+def get_single_workspace(workspace_id):
+    workspace = Workspace.query.get(workspace_id)
+    return workspace.to_dict_simple()
+
+
+# * -----------  GET  --------------
 # Returns all Workspaces that the user is a part of
 
 @workspace_routes.route('')
@@ -17,3 +26,5 @@ def get_all_workspaces():
     user = User.query.filter(User.id == current_user.id).first()
     workspaces = user.joined_workspaces
     return [workspace.to_dict_simple() for workspace in workspaces]
+
+
