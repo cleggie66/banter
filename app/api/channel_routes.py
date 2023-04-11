0543a725edb4973f5e8596da331e509386ef45ca
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-04-10 21:29:15 -0700

    merge attempt fail

--- app/api/channel_routes.py
+++ app/api/channel_routes.py
@@ -12,7 +12,7 @@ channel_routes = Blueprint('channels', __name__)
 # * -----------  GET  --------------
 #  Returns the details of a channel specified by its Id
 
-@channel_routes.route("/<int:channel_id>")
+@channel_routes.route("/<channel_id>")
 @login_required
 def get_single_channels(channel_id):
     channel = Channel.query.get(channel_id)
@@ -55,7 +55,7 @@ def create_channel():
 
 
 # TODO -----------  PUT  --------------
-@channel_routes.route('/<int:channel_id>', methods=['PUT','PATCH'])
+@channel_routes.route('/<channel_id>', methods=['PUT','PATCH'])
 @login_required
 def update_channel(channel_id):
     edit = request.json
