8d408fe7fa8f98acdc57b945dd2328fbeca6fe72
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-04-13 15:38:15 -0700

    workspace refactor now only shows workspaces you are a member of

--- react-app/src/components/Workspaces/WorkspacesIndex/index.js
+++ react-app/src/components/Workspaces/WorkspacesIndex/index.js
@@ -3,25 +3,31 @@ import { useSelector, useDispatch } from "react-redux";
 import { getAllWorkspacesThunk } from "../../../store/workspace";
 import WorkspaceCard from "./WorkspaceCard";
 import { clearActiveChannelThunk } from "../../../store/activeChannel";
-import './WorkSpacesIndex.css'
+import { refreshUser } from "../../../store/session";
+import "./WorkSpacesIndex.css";
 
 const WorkspacesIndex = () => {
   const sessionUser = useSelector((state) => state.session.user);
+  const allJoinedWorkspaces = useSelector(
+    (state) => state.session.user.joined_workspaces
+  );
 
   const dispatch = useDispatch();
 
-  const allWorkspaces = useSelector((state) => Object.values(state.workspaces));
+  // every workspace ever made
+  // const allWorkspaces = useSelector((state) => Object.values(state.workspaces));
 
   useEffect(() => {
     dispatch(getAllWorkspacesThunk());
     dispatch(clearActiveChannelThunk());
+    dispatch(refreshUser(sessionUser.id));
   }, [dispatch]);
 
   return (
     <>
       <h1>Your Workspaces</h1>
       <div className="workspace-list-area">
-        {allWorkspaces.map((workspace) => (
+        {allJoinedWorkspaces.map((workspace) => (
           <WorkspaceCard key={workspace.id} workspace={workspace} />
         ))}
       </div>
