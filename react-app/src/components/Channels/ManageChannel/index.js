4388ee8969ec4448e0e6578d38b0cee66efc1b2a
JakeG97 <jgularteee@gmail.com>
2023-04-13 11:09:06 -0700

    More styling for clear functionality

--- react-app/src/components/Channels/ManageChannel/index.js
+++ react-app/src/components/Channels/ManageChannel/index.js
@@ -26,9 +26,11 @@ function ManageChannelModal({ workspaceId }) {
 
   return (
     <div className="channel-modal-container">
+      <div className="channel-modal-border">
         <h3 id="channel-modal-form"className="title-text">Create a channel</h3>
         <p className="title-text">Get the Banter started!</p>
         <button id ="modal-buttons"className="create-channel-button"onClick={handleAddChannel}>Create</button>
+      </div>
         <h3 className="title-text">Manage your channels</h3>
         <p className="title-text">Edit your channels or delete<br/>them and get rid of the Banter!</p>
         <button id="modal-buttons" className="create-channel-button" onClick={handleManageChannel}>Manage</button>
