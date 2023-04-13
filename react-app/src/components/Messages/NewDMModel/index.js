388b05afdb2e31d8b2f77b4c17a5dbe8ef9f7d00
Caleb Cleghorn <caleb@cleghorn.org>
2023-04-13 18:07:30 -0500

    direct messages work

--- react-app/src/components/Messages/NewDMModel/index.js
+++ react-app/src/components/Messages/NewDMModel/index.js
@@ -4,31 +4,78 @@ import { useHistory } from "react-router-dom";
 import { useModal } from "../../../context/Modal";
 import { signUp } from "../../../store/session";
 import { getWorkspaceByIdThunk } from "../../../store/workspace";
+import { addUserToChannelThunk, createChannelThunk } from "../../../store/channel";
 import "./NewDMModel.css"
 
 function NewDMModal({ workspaceId }) {
     const dispatch = useDispatch();
+    const [recipients, setRecipients] = useState([])
 
     useEffect(() => {
         dispatch(getWorkspaceByIdThunk(workspaceId));
     }, [dispatch, workspaceId]);
 
-    
+
     const activeWorkspaceState = useSelector((state) => state.workspaces);
     const activeWorkspace = activeWorkspaceState[workspaceId];
-    
+    const users = activeWorkspace.users_in_workspaces
+
     const { closeModal } = useModal();
-    
-    const handleSubmit = async (e) => {
-        
+
+    const handleClick = async (e) => {
+
     };
-    
-    console.log(activeWorkspace)
+
+    const addUsers = async (channelId) => {
+        recipients.forEach((user) => {
+            addUserToChannelThunk(user.id, channelId)
+        })
+    }
+
+    // TODO: DEBUG
+    const handleSubmit = async () => {
+        const channelInformation = {
+            name: "direct message",
+            workspace_id: Number(workspaceId),
+            is_channel: false,
+        };
+
+        let newChannel = await dispatch(createChannelThunk(channelInformation));
+
+        addUsers(newChannel.id)
+
+        closeModal()
+    }
+
     return (
         <>
             <div className="new-message-container">
                 <h1>New Message</h1>
-                <button type="submit">Create Message</button>
+                <div className="recipients-list">
+                    {recipients.map((user) => (
+                        <h4>{`${user.first_name} ${user.last_name}`}</h4>
+                    ))}
+                </div>
+                <div className="all-users-list">
+                    {users.map((user) => (
+                        <div
+                            className="user-in-list"
+                            onClick={() => {
+                                setRecipients([...recipients, user])
+                            }}
+                        >
+                            <div className='users-list-image-container'>
+                                <img
+                                    src={user.profile_picture}
+                                    alt="profile"
+                                    className="users-list-profile-pic"
+                                />
+                            </div>
+                            <h4>{`${user.first_name} ${user.last_name}`}</h4>
+                        </div>
+                    ))}
+                </div>
+                <button onClick={handleSubmit}>Create Message</button>
             </div>
         </>
     );
