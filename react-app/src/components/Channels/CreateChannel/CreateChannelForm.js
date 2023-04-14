ce6542cb8cedae4d9fd711a23b155f403ef7fb07
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-04-14 14:54:34 -0700

    removed extra logs

--- react-app/src/components/Channels/CreateChannel/CreateChannelForm.js
+++ react-app/src/components/Channels/CreateChannel/CreateChannelForm.js
@@ -39,7 +39,6 @@ function CreateChannelForm({ workspaceId }) {
         workspace_id: Number(workspaceId),
         is_channel: true,
       };
-      console.log(channelInformation);
 
       let newChannel = await dispatch(createChannelThunk(channelInformation));
       dispatch(refreshUser(sessionUser.id));
