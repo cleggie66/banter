2a23044dd7914efcb81be333b694cd92e73c4c3c
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-04-15 14:07:05 -0700

    dms working again

--- react-app/src/components/Messages/NewDMModel/index.js
+++ react-app/src/components/Messages/NewDMModel/index.js
@@ -2,135 +2,114 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useModal } from "../../../context/Modal";
 import { getWorkspaceByIdThunk } from "../../../store/workspace";
-import { addUserToChannelThunk, createChannelThunk } from "../../../store/channel";
-import "./NewDMModel.css"
+import {
+  addUserToChannelThunk,
+  createChannelThunk,
+} from "../../../store/channel";
+import "./NewDMModel.css";
 import { loadActiveChannel } from "../../../store/activeChannel";
 
 function NewDMModal({ workspaceId }) {
 
-    const activeChannel = useSelector((state) => state.activeChannel);
-    
+  const dispatch = useDispatch();
 
+  const [recipients, setRecipients] = useState(new Set());
 
+  useEffect(() => {
+    dispatch(getWorkspaceByIdThunk(workspaceId));
+  }, [dispatch, workspaceId]);
 
+  const activeWorkspaceState = useSelector((state) => state.workspaces);
+  const sessionUser = useSelector((state) => state.session.user);
+  const channels = useSelector((state) => Object.values(state.channels));
 
+  const activeWorkspace = activeWorkspaceState[workspaceId];
+  const allUsers = activeWorkspace.users_in_workspaces;
+  const users = allUsers.filter((user) => user.id !== sessionUser.id);
 
-    const dispatch = useDispatch();
+  const { closeModal } = useModal();
 
+  const handleSubmit = async () => {
+    const channelInformation = {
+      name: "name",
+      workspace_id: Number(workspaceId),
+      is_channel: false,
+    };
 
-    const [recipients, setRecipients] = useState(new Set())
+    // checks if channel already exists — might need ot be refactored
+    const existingChannels = new Set();
+    channels.forEach((channel) => {
+      let userIds = [];
+      channel.users_in_channels.forEach((user) => {
+        userIds.push(user.id);
+      });
+      let sortedIds = userIds.sort();
+      existingChannels.add(sortedIds.join(""));
+    });
 
+    let proposedId = [];
+    let recipArray = [...recipients];
+    recipArray.forEach((user) => {
+      proposedId.push(user.id);
+    });
+    proposedId.push(sessionUser.id);
+    let sortedProposedId = proposedId.sort().join("");
 
+    if (existingChannels.has(sortedProposedId)) {
+      closeModal();
+    } else {
+      let newChannel = await dispatch(createChannelThunk(channelInformation));
 
+      recipients.forEach(async (user) => {
+        await dispatch(addUserToChannelThunk(user.id, newChannel.id));
+      });
 
-    useEffect(() => {
-        dispatch(getWorkspaceByIdThunk(workspaceId));
-    }, [dispatch, workspaceId]);
+      dispatch(loadActiveChannel(newChannel.id));
 
-
-    const activeWorkspaceState = useSelector((state) => state.workspaces);
-    const sessionUser = useSelector((state) => state.session.user);
-    const channels = useSelector((state) => Object.values(state.channels));
-
-    const activeWorkspace = activeWorkspaceState[workspaceId];
-    const allUsers = activeWorkspace.users_in_workspaces
-    const users = allUsers.filter((user) => user.id !== sessionUser.id)
-
-    const { closeModal } = useModal();
-
-    const handleSubmit = async () => {
-        const channelInformation = {
-            name: "name",
-            workspace_id: Number(workspaceId),
-            is_channel: false,
-        };
-
-        // checks if channel already exists — might need ot be refactored
-        const existingChannels = new Set();
-        channels.forEach((channel) => {
-            let userIds = [];
-            channel.users_in_channels.forEach((user) => {
-                userIds.push(user.id)
-            })
-            let sortedIds = userIds.sort()
-            existingChannels.add(sortedIds.join(""))
-        })
-
-        let proposedId = []
-        let recipArray = [...recipients]
-        recipArray.forEach((user) => {
-            proposedId.push(user.id)
-        })
-        proposedId.push(sessionUser.id)
-        let sortedProposedId = proposedId.sort().join("")
-
-        if (existingChannels.has(sortedProposedId)) {
-            closeModal()
-        } else {
-            let newChannel = await dispatch(createChannelThunk(channelInformation));
-
-            recipients.forEach(async (user) => {
-                await dispatch(addUserToChannelThunk(user.id, newChannel.id))
-            })
-
-            dispatch(loadActiveChannel(newChannel.id))
-
-            closeModal()
-        }
+      closeModal();
     }
+  };
 
-
-    // const handleCardClick = (e) => {
-    //     e.preventDefault();
-    //     dispatch(getChannelByIdThunk(channel.id));
-    //     dispatch(getAllChannelMessagesThunk(channel.id));
-    //     dispatch(loadActiveChannel(channel.id));
-    //     history.push(`/dashboard/${channel.workspace_id}/${channel.name}`);
-    //   };
-
-
-
-
-
-
-
-    return (
-        <>
-            <div className="new-message-container">
-                <h1 className="title-text">New Message</h1>
-                <div className="title-text">
-                    {[...recipients].map((user) => (
-                        <h4>{`${user.first_name} ${user.last_name}`}</h4>
-                    ))}
-                </div>
-                <div className="all-users-list">
-                    {users.map((user) => (
-                        <div
-                            className="user-in-list"
-                            onClick={() => {
-                                setRecipients(new Set([...recipients, user]))
-                            }}
-                        >
-                            <div className='users-list-image-container'>
-                                <img
-                                    src={user.profile_picture}
-                                    alt="profile"
-                                    className="users-list-profile-pic"
-                                />
-                            </div>
-                            <h4 className="title-text">{`${user.first_name} ${user.last_name}`}</h4>
-                        </div>
-                    ))}
-                </div>
-                <div className="create-message-button-container">
-                    {recipients ? (
-                        <button className="create-channel-button" onClick={handleSubmit}>Create Message</button>
-                    ) : <h2>No other users in this workspace!</h2>}
-
-                </div>
+  return (
+    <>
+      <div className="new-message-container">
+        <h1 className="title-text">New Message</h1>
+        <div className="title-text">
+          {[...recipients].map((user) => (
+            <h4>{`${user.first_name} ${user.last_name}`}</h4>
+          ))}
+        </div>
+        <div className="all-users-list">
+          {users.map((user) => (
+            <div
+              className="user-in-list"
+              onClick={() => {
+                setRecipients(new Set([...recipients, user]));
+              }}
+            >
+              <div className="users-list-image-container">
+                <img
+                  src={user.profile_picture}
+                  alt="profile"
+                  className="users-list-profile-pic"
+                />
+              </div>
+              <h4 className="title-text">{`${user.first_name} ${user.last_name}`}</h4>
             </div>
-        </>
-    );
+          ))}
+        </div>
+        <div className="create-message-button-container">
+          {recipients ? (
+            <button className="create-channel-button" onClick={handleSubmit}>
+              Create Message
+            </button>
+          ) : (
+            <h2>No other users in this workspace!</h2>
+          )}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default NewDMModal;
