82e182e3b2dfca1587bbe869527e5c33ede72999
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-04-15 22:16:54 -0700

    removed even more warnings

--- react-app/src/components/Channels/UpdateChannel/ChannelDisplay.js
+++ react-app/src/components/Channels/UpdateChannel/ChannelDisplay.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import OpenModalButton from "../../OpenModalButton";
 import DeleteChannelModal from "../DeleteChannel";
 import EditChannelModal from "./EditChannelModal";
