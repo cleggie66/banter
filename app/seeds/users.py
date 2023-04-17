84ff9fc01b61ac167c1d0c0aa6372f5fa2a3324a
StevenbradleyA <andersonstevenbradley@gmail.com>
2023-04-16 18:11:16 -0700

    created new users for seeder file

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -9,11 +9,42 @@ alec = User(
     username='Alec', password='password', email='mod4god@aa.io', first_name="Alec", last_name="Instructor", profile_picture="https://i0.wp.com/d37ck3jytu9wl0.cloudfront.net/wp-content/uploads/2022/05/31141519/Neckbeard-BLOG.jpg?fit=1200%2C777&ssl=1", title="Mod 4 Gate Keeper", about_me="I am the greatest mod 4 instructor of all time")
 brad = User(
     username='Brad',  password='password', email='Brad@aa.io', first_name="Brad", last_name="Instructor", profile_picture="https://cdn.vox-cdn.com/thumbor/qqJR3THHUSgAt2ADTGk6_56hjyQ=/0x0:4059x4051/1400x1400/filters:focal(1964x2333:1965x2334)/cdn.vox-cdn.com/uploads/chorus_asset/file/24018802/ND_Zelda_Lead.jpeg", title="Food variable master", about_me="The father of Patches and Blue")
+spongebob = User(
+    username='spongebob',  password='password', email='spongebob@aa.io', first_name="Spongebob", last_name="Squarepants", profile_picture="https://www.freepnglogos.com/uploads/spongebob-png/ish-twitter-quot-redrew-favorite-scene-spongebob-9.png", title="Fry Cook", about_me="I'm a joyful sea sponge who lives in a pineapple with my pet snail gary") 
+patrick = User(
+    username='patrick',  password='password', email='patrick@aa.io', first_name="Patrick", last_name="Star", profile_picture="https://upload.wikimedia.org/wikipedia/en/thumb/3/33/Patrick_Star.svg/1200px-Patrick_Star.svg.png", title="Dr Professor Patrick to you", about_me="I live under a rock in Bikini Bottom. Hmu for embarrassing photos of Spongebob at last years christmas party") 
+mr_krabs = User(
+    username='MrKrabs',  password='password', email='MrKrabs@aa.io', first_name="Mr", last_name="Krabs", profile_picture="", title="CEO of the Krusty Krab", about_me="Hi, I like money.") 
+squidward = User(
+    username='patrick',  password='password', email='patrick@aa.io', first_name="Patrick", last_name="Star", profile_picture="", title="Dr Professor Patrick to you", about_me="I live under a rock in Bikini Bottom. Hmu for embarrassing photos of Spongebob at last years christmas party") 
+sandy = User(
+    username='patrick',  password='password', email='patrick@aa.io', first_name="Patrick", last_name="Star", profile_picture="", title="Dr Professor Patrick to you", about_me="I live under a rock in Bikini Bottom. Hmu for embarrassing photos of Spongebob at last years christmas party") 
+larry = User(
+    username='patrick',  password='password', email='patrick@aa.io', first_name="Patrick", last_name="Star", profile_picture="", title="Dr Professor Patrick to you", about_me="I live under a rock in Bikini Bottom. Hmu for embarrassing photos of Spongebob at last years christmas party") 
+bubble_bass = User(
+    username='patrick',  password='password', email='patrick@aa.io', first_name="Patrick", last_name="Star", profile_picture="", title="Dr Professor Patrick to you", about_me="I live under a rock in Bikini Bottom. Hmu for embarrassing photos of Spongebob at last years christmas party") 
+man_ray = User(
+    username='patrick',  password='password', email='patrick@aa.io', first_name="Patrick", last_name="Star", profile_picture="", title="Dr Professor Patrick to you", about_me="I live under a rock in Bikini Bottom. Hmu for embarrassing photos of Spongebob at last years christmas party") 
+
 
 def seed_users():
     db.session.add(demo)
     db.session.add(alec)
     db.session.add(brad)
+    db.session.add(spongebob)
+    db.session.add(patrick)
+    db.session.add(mr_krabs)
+    db.session.add(squidward)
+    db.session.add(sandy)
+    db.session.add(larry)
+    db.session.add(bubble_bass)
+    db.session.add(man_ray)
+
+
+
+
+
+
     db.session.commit()
 
 
